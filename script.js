062e665e0314d1fd88b59ca97d0c27e66a88326f
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-04-10 00:45:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+let button = document.querySelector('button');
+
+button.addEventListener('click',()=>{
+	let f = document.querySelector('#id1').value;
+	let l = document.querySelector('#id2').value;
+	let p = document.querySelector('#id3').value;
+	let e = document.querySelector('#id4').value;
+	
+	alert(`First Name:${f} Last Name:${l} Phone Number:${p} Email ID:${e}`);
+})
