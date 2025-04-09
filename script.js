aff812f8db4ec3aad8842e3133fff256f88e19a9
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-04-10 00:57:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ let form = document.querySelector('form');
 let f = document.querySelector('#id1');
 	let l = document.querySelector('#id2');
 	let p = document.querySelector('#id3');
-	let e = document.querySelector('#id4');
-form.addEventListener('submit',(e)=>{
+	let email = document.querySelector('#email');
+	form.addEventListener('submit',(e)=>{
 	e.preventDefault();
 	let first = f.value;
 	let last = l.value;
 	let phone = p.value;
-	let email = e.value;
+	let emailid = email.value;
 	
-	alert(`First Name: ${first} Last Name: ${last} Phone Number: ${phone} Email ID: ${email}`);
+	alert(`First Name: ${first} Last Name: ${last} Phone Number: ${phone} Email ID: ${emailid}`);
 })
